6bade8c34c5855a93105949018f64404d92e5220
chyacin <calvinhyacinth@gmail.com>
2021-02-20 10:01:04 +0100

    Created the String for the get Attractions api

--- src/main/java/tourGuide/utils/GpsUtilFinal.java
+++ src/main/java/tourGuide/utils/GpsUtilFinal.java
@@ -7,4 +7,7 @@ public class GpsUtilFinal {
 
     public static final String GPS_GET_USER_LOCATION_URL = "http://localhost:7080/userLocation";
 
+    public static final String GPS_GET_ATTRACTION_URL = "http://localhost:7080/allAttractions";
+
+
 }

c6869eb2e6cb49c6067e135e20a8a34c4c3c48f9
chyacin <calvinhyacinth@gmail.com>
2021-02-22 14:14:18 +0100

    create the class for storing the api for rewards central

---
+++ src/main/java/tourGuide/utils/RewardsCentralFinal.java
@@ -0,0 +1,7 @@
+package tourGuide.utils;
+
+public class RewardsCentralFinal {
+
+    public static final String REWARDS_GET_ATTRACTION_REWARD_POINTS_URL = "http://localhost:6080/attractionRewardPoints";
+
+}
